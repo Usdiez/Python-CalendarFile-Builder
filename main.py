a12b13da8e3fea5fc5da93a173e096eea70e809b
Usdiez <50532212+Usdiez@users.noreply.github.com>
2021-02-12 16:42:47 -0500

    Everything working as intended

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Button, Frame, Grid, messagebox
+from tkinter import StringVar, Tk, Button, Frame, messagebox, OptionMenu
 import tkinter as tk
 from ics import Calendar, Event
 from datetime import date
@@ -44,22 +44,28 @@ def createCalender():
     #Iniltilizes Calender
     eCalender = Calendar()
 
-    #Change event dates to 00 format
+    #Change event days to 00 format
     fixedEventDates = ToggleButton.getDates()
 
     for i in range(len(fixedEventDates)):
         if len(fixedEventDates[i]) == 1:
             fixedEventDates[i] = '0'+fixedEventDates[i]
 
-    #Give current month and year for events
+    #Change event months to 00 format
+
+    monthNum = str(monthOptions.index(monthVar.get())+1)
+    if len(monthNum) == 1:
+        monthNum = '0'+monthNum
+    
+    #Give current year for events
     today = date.today()
-    temptime = today.strftime("%Y-%m")
+    temptime = today.strftime("%Y-")
 
     #Add dates to list
     eventsList = []
     for i in range(len(fixedEventDates)):
         try:
-            eventsList.append(Event(name="test event", begin= temptime+"-"+fixedEventDates[i]+" 00:00:00"))
+            eventsList.append(Event(name=eventName.get(), begin= temptime+monthNum+"-"+fixedEventDates[i]+" 00:00:00"))
         except ValueError:
             tk.messagebox.showwarning(title='Incorrect dates for month', message='Selected dates are out of the selected month\'s range')
 
@@ -88,14 +94,26 @@ for i in range(3):
     buttons[C].grid(row=4,column=i)
     C=C+1
 
-#List Test Button
-buttons.append(Button(root,text='Print List', bg='yellow', command = createCalender))
-buttons[-1].grid(row=7)
+#Runs 
+runButton = Button(root,text='Create Event', bg='yellow', command = createCalender)
+runButton.grid(row=7)
+
+#Month Dropdown Selection
+
+monthOptions = ['January','Feburary','March','April','May','June','July','August','September','October','November','December']
+monthVar = StringVar(root)
+monthVar.set(monthOptions[0])
+monthDropdown = OptionMenu(root, monthVar, *monthOptions)
+monthDropdown.grid(row=8)
 
 
+#Event Name Selection
 
-
-    #eventStartDate
+eventNameLabel = tk.Label(root,text= 'Enter event name:')
+eventNameLabel.grid(row=9)
+eventName = tk.StringVar()
+eventNameBox = tk.Entry(root, width=18, textvariable= eventName)
+eventNameBox.grid(row=10)
 
 
 
